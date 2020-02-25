67f103158f1a4f67f43163c3393d210a507ff1bd
Mostafa <mostafa.sedaghat@gmail.com>
2020-02-25 16:05:10 +0800

    Add GetSignBytes method for multisig

---
+++ types/stdtx.go
@@ -0,0 +1,26 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/exported"
+	sdkAuth "github.com/cosmos/cosmos-sdk/x/auth/types"
+)
+
+func GetSignBytes(ctx sdk.Context, tx sdkAuth.StdTx, acc exported.Account) []byte {
+	genesis := ctx.BlockHeight() == 0
+	chainID := ctx.ChainID()
+	var accNum uint64
+	if !genesis {
+		accNum = acc.GetAccountNumber()
+	}
+
+	seq := acc.GetSequence()
+	if acc.IsMultiSig() {
+		// 1- check if tx exists in pending tx, then tx_id is same as pending.id
+		// 2. if not, seq = acc.GetCounter()
+	}
+
+	return sdkAuth.StdSignBytes(
+		chainID, accNum, seq, tx.Fee, tx.Msgs, tx.Memo,
+	)
+}
