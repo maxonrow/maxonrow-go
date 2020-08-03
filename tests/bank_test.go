e7d101525ab5b85f88aece7fd4bf7495a4481141
Tan Yen Khoon <tankhoon@gmail.com>
2020-08-03 16:02:16 +0800

    fix test case.

--- tests/bank_test.go
+++ tests/bank_test.go
@@ -12,8 +12,8 @@ func makeBankTxs() []*testCase {
 
 		{"bank", false, false, "sending 1 cin", "alice", "200000000cin", 0, bankInfo{"alice", "bob", "1cin"}, "", nil},
 		{"bank", true, true, "sending 0 cin", "alice", "200000000cin", 0, bankInfo{"alice", "bob", "0cin"}, "", nil},
-		{"bank", true, true, "insufficient amount", "carlo", "1000000000cin", 0, bankInfo{"carlo", "eve", "999999999999000000001cin"}, "", nil},
-
+		{"bank", true, true, "insufficient amount", "carlo", "1000000000cin", 0, bankInfo{"carlo", "eve", "99999999999999999999999000000001cin"}, "", nil},
+		                                                                                                                                       
 		{"bank", false, false, "transffer all coins", "gohck", "1000000000cin", 0, bankInfo{"gohck", "eve", "999999999999000000000cin"}, "", nil},
 
 		{"bank", true, true, "sending 1 abc", "alice", "200000000cin", 0, bankInfo{"alice", "bob", "1abc"}, "", nil},
